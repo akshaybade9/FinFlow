865f8044ea3d98a839d08c02e9ad5e5822e074a0
Akshay Bade <117570685+akshaybade9@users.noreply.github.com>
2026-03-08 22:07:24 +0530

    Modify actuator endpoints scope
    Modify actuator endpoints scope

--- finflow-parent-pom/finflow-customer-service/src/main/java/com/finflow/customer/controller/CustomerController.java
+++ finflow-parent-pom/finflow-customer-service/src/main/java/com/finflow/customer/controller/CustomerController.java
@@ -4,12 +4,17 @@ package com.finflow.customer.controller;
 import com.finflow.customer.entity.Customer;
 import com.finflow.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
+	
+	private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
 
     private final CustomerService customerService;
 
@@ -20,6 +25,7 @@ public class CustomerController {
 
     @GetMapping("/{id}")
     public Customer getCustomer(@PathVariable("id") Long id) {
+    	 log.debug("Fetching customer");
         return customerService.getCustomer(id);
     }
 }
